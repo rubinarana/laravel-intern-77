1f3e1e4993128f129dcad1d40e4a8f1174eb5881
rubinarana <rubinaranamgr@gmail.com>
2020-05-04 19:08:19 +0545

    post

--- app/User.php
+++ app/User.php
@@ -37,18 +37,23 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function followers()
-    {
-        return $this->hasMany(Followers::class, 'user_id');
-    }
+    // public function followers()
+    // {
+    //     return $this->hasMany(Followers::class, 'user_id');
+    // }
 
-    public function following()
-    {
-        return $this->hasMany(Followers::class, 'follower_id');
-    }
+    // public function following()
+    // {
+    //     return $this->hasMany(Followers::class, 'follower_id');
+    // }
 
-    public function follows(User $user)
+    // public function follows(User $user)
+    // {
+    //     return Followers::where('user_id', $user->id)->where('follower_id', auth()->user()->id)->first() ? true : false; 
+    // }
+
+     public function posts()
     {
-        return Followers::where('user_id', $user->id)->where('follower_id', auth()->user()->id)->first() ? true : false; 
+        return $this->hasMany(Post::class);
     }
 }
