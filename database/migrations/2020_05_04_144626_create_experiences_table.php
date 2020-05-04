8cedb7b9d2ce1cc148e029091938c792148c1351
SlokBaniya <slokbaniya1@gmail.com>
2020-05-04 22:03:53 +0545

    Create Model 'Experiences', 'Education Details', 'Social Links'

---
+++ database/migrations/2020_05_04_144626_create_experiences_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExperiencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('experiences', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('work_place');
+            $table->date('started_date');
+            $table->date('end_date');
+            $table->foreign('user_id')->references('id')->on('users');
+           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('experiences');
+    }
+}
