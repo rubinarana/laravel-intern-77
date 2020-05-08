b0a999d5c4ad63452e8e6fa34c0ee6bc6dad8712
BinayMdr <binaymdr25@gmail.com>
2020-05-08 09:00:39 +0545

    UserProfileForm

--- app/Http/Controllers/IndexController2.php
+++ app/Http/Controllers/IndexController2.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\userprofile;
 use Illuminate\Http\Request;
 
 class IndexController2 extends Controller
@@ -9,5 +9,26 @@ class IndexController2 extends Controller
     public function users() {
         $users = \App\User::paginate(10);
         return view('users_view')->with('users', $users);
+    }
+
+    public function userprofile(Request $request)
+    {
+        $data=request()->validate([
+         'Address'=>'required',
+         'Contact'=>'required',
+         'Bio'=>'required|min:20'
+        ]);
+        $user_id=auth()->user()->id;
+        $address=$request->input('Address');
+        $contact=$request->input('Contact');
+        $bio=$request->input('Bio');
+        UserProfile::firstOrCreate([
+            'user_id' => $user_id,
+            'address'=>$address,
+            'contact_no'=>$contact,
+            'bio'=>$bio
+        ]);
+        return back();
+
     }
 }
